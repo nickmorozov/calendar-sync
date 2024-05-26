2e809370f4755ae1bf5b6a398cee020a7c169018
Nick Morozov <neurodeep@gmail.com>
2024-05-26 11:13:27 -0700

    Initial Commit

---
+++ calendar-sync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calendar-sync
+//
+//  Created by Nick Morozov on 2024-05-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calendar-sync/calendar_syncApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendar_syncApp.swift
+//  calendar-sync
+//
+//  Created by Nick Morozov on 2024-05-26.
+//
+
+import SwiftUI
+
+@main
+struct calendar_syncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
